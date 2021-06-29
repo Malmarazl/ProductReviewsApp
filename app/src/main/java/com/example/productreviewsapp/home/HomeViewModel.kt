735098fb5f5ffb6e4c693b4e1bf37c7cee5e9af2
Malmaraz <monicalmaraz91@gmail.com>
2021-06-29 09:26:50 +0200

    Add retrofit library and implementation

--- app/src/main/java/com/example/productreviewsapp/home/HomeViewModel.kt
+++ app/src/main/java/com/example/productreviewsapp/home/HomeViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.productreviewsapp.home
 
-class HomeViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.productreviewsapp.models.Product
+import com.example.productreviewsapp.services.ServiceAdapter
+import kotlinx.coroutines.launch
+
+class HomeViewModel : ViewModel() {
+
+    val productList = MutableLiveData<List<Product>>()
+
+    fun getProducts() {
+        viewModelScope.launch {
+            connectionToProductService()
+        }
+    }
+
+    private suspend fun connectionToProductService() {
+        val response = ServiceAdapter().getApiService()?.getProduct()
+        productList.postValue(response)
+    }
+
 }
